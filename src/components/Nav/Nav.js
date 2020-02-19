5c72f6f2438a829bd2e8e86b0778e22694a4187b
Fauzan <fauzan_lubis23@yahoo.com>
2020-02-19 16:26:42 +0800

    update nav bar

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -93,9 +93,9 @@ class Nav extends React.Component {
                 <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width="24" height="24" viewBox="0 0 24 24"><path d="M20 11H7.83l5.59-5.59L12 4l-8 8 8 8 1.41-1.41L7.83 13H20v-2z"/></svg>
               </span>
               
-              <span style={{fontSize:20, fontWeight:'bold'}} className="title mdc-toolbar__title">{this.state.title}</span>
+              <span style={{fontSize:21, fontWeight:'bold'}} className="title mdc-toolbar__title">{this.state.title}</span>
             </section>
-            <section className="mdc-toolbar__section mdc-toolbar__section--align-end" role="toolbar">
+            <section  role="toolbar">
               <div className="mdc-menu-anchor">
                 <span className="mdc-toolbar__icon mdc-toolbar__icon--menu simpleMenu" aria-label="Simple menu"  onClick={() => this.toggleMenu()}>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" width="24" height="24" viewBox="0 0 24 24"><path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"/></svg>
